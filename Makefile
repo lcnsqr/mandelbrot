3cae59e7b2a21c65861614d746aadf1157be454f
Luciano Siqueira <lcnsqr@gmail.com>
2018-07-09 12:07:51 -0300

    Paralelização alterada para openmp

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ OPTIONS=-O3
 all: mandelbrot
 
 mandelbrot: mandelbrot.c
-	$(CC) mandelbrot.c -lm -lpthread `sdl2-config --cflags --libs` $(OPTIONS) -o $@
+	$(CC) mandelbrot.c -lm -fopenmp `sdl2-config --cflags --libs` $(OPTIONS) -o $@
 
 clean:
 	rm mandelbrot
--- mandelbrot.c
+++ mandelbrot.c
@@ -3,15 +3,14 @@
 #include <math.h>
 #include "SDL.h"
 #include "color.h"
-#include <pthread.h>
 
 // Parâmetros iniciais
 #define XMIN -2.5
 #define XMAX 1
-#define YMIN -1
-#define YMAX 1
+#define YMIN -1.75
+#define YMAX 1.75
 #define ITERATIONS 1000
-#define WIDTH 700
+#define WIDTH 400
 #define HEIGHT 400
 #define DEPTH 4
 
@@ -63,46 +62,15 @@ double mandelIter(double cx, double cy, int iterations){
 	return (double)i;
 }
 
-void *renderPart(void *vCtx){
-	struct Context *ctx = (struct Context *)vCtx;
-	// Coordenadas na imagem
-	int ix, iy;
-	// Coordenadas do ponto candidato
-	double x, y;
-	// Cor
-	unsigned int c, rgba[4];
-	for (int i = ctx->view.frameSize/2; i < ctx->view.frameSize; i += ctx->view.depth){
-		ix = (i/ctx->view.depth) % ctx->view.width;
-		iy = (i/ctx->view.depth) / ctx->view.width;
-		// A orientação vertical é invertida na imagem
-		iy = ctx->view.height - iy - 1;
-      x = ctx->xmin + (ctx->xmax - ctx->xmin) * ix / (ctx->view.width - 1);
-      y = ctx->ymin + (ctx->ymax - ctx->ymin) * iy / (ctx->view.height - 1);
-		c = floor((double)ITERATIONS * log(mandelIter(x, y, ctx->iterations) )/log(ITERATIONS+1));
-		corMap(&ctx->view.cor, c, rgba);
-		ctx->view.frame[i] = (char)rgba[0];
-		ctx->view.frame[i+1] = (char)rgba[1];
-		ctx->view.frame[i+2] = (char)rgba[2];
-		ctx->view.frame[i+3] = (char)rgba[3];
-	}
-	return NULL;
-}
-
 void render(struct Context *ctx){
-	// Thread da segunda metade da image
-	pthread_t render_thread;
-	if(pthread_create(&render_thread, NULL, renderPart, ctx)) {
-		fprintf(stderr, "Error creating thread\n");
-		return;
-	}
-
 	// Coordenadas na imagem
 	int ix, iy;
 	// Coordenadas do ponto candidato
 	double x, y;
 	// Cor
 	unsigned int c, rgba[4];
-	for (int i = 0; i < ctx->view.frameSize/2; i += ctx->view.depth){
+	#pragma omp parallel for private(ix,iy,x,y,c,rgba)
+	for (int i = 0; i < ctx->view.frameSize; i += ctx->view.depth){
 		ix = (i/ctx->view.depth) % ctx->view.width;
 		iy = (i/ctx->view.depth) / ctx->view.width;
 		// A orientação vertical é invertida na imagem
@@ -115,11 +83,6 @@ void render(struct Context *ctx){
 		ctx->view.frame[i+1] = (char)rgba[1];
 		ctx->view.frame[i+2] = (char)rgba[2];
 		ctx->view.frame[i+3] = (char)rgba[3];
-	}
-	// wait for the second thread to finish
-	if(pthread_join(render_thread, NULL)) {
-		fprintf(stderr, "Error joining thread\n");
-		return;
 	}
 	
 	// Atualizar exibição
@@ -130,108 +93,119 @@ void render(struct Context *ctx){
 	SDL_RenderPresent(ctx->renderer);
 }
 
-int main(){
-	// Contexto de renderização
-	struct Context ctx;
-
-	ctx.xmin = XMIN;
-	ctx.xmax = XMAX;
-	ctx.ymin = YMIN;
-	ctx.ymax = YMAX;
-	ctx.iterations = ITERATIONS;
-	// Estrutura da imagem
-	ctx.view.width = WIDTH;
-	ctx.view.height = HEIGHT;
-	ctx.view.depth = DEPTH;
-	ctx.view.frameSize = ctx.view.width*ctx.view.height*ctx.view.depth;
-	ctx.view.frame = (char *)malloc(sizeof(char)*ctx.view.frameSize);
-	// Total de cores equivale ao números de iterações
-	corBuild(&ctx.view.cor, ctx.iterations);
+int main(int argc, char **argv){
+	// Contextos de renderização
+	// 0: Mandelbrot pelo algoritmo tradicional
+	// 1: Estimado pela GRNN
+	struct Context ctx[2];
+	for (int c = 0; c < 2; c++){
+		ctx[c].xmin = XMIN;
+		ctx[c].xmax = XMAX;
+		ctx[c].ymin = YMIN;
+		ctx[c].ymax = YMAX;
+		ctx[c].iterations = ITERATIONS;
+		// Estrutura da imagem
+		ctx[c].view.width = WIDTH;
+		ctx[c].view.height = HEIGHT;
+		ctx[c].view.depth = DEPTH;
+		ctx[c].view.frameSize = ctx[c].view.width*ctx[c].view.height*ctx[c].view.depth;
+		ctx[c].view.frame = (char *)malloc(sizeof(char)*ctx[c].view.frameSize);
+		// Total de cores equivale ao números de iterações
+		corBuild(&ctx[c].view.cor, ctx[c].iterations);
+	}
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Erro ao iniciar o SDL: %s", SDL_GetError());
 		exit(-1);
 	}
 
-	if (SDL_CreateWindowAndRenderer(ctx.view.width, ctx.view.height, SDL_WINDOW_RESIZABLE, &ctx.window, &ctx.renderer)) {
+	// Mandelbrot
+	if (SDL_CreateWindowAndRenderer(ctx[0].view.width, ctx[0].view.height, 0, &ctx[0].window, &ctx[0].renderer)) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Impossível criar a janela e o renderizador: %s", SDL_GetError());
 		exit(-1);
 	}
-	SDL_SetWindowTitle(ctx.window, "Conjunto Mandelbrot");
+	SDL_SetWindowTitle(ctx[0].window, "Mandelbrot");
+	ctx[0].texture = SDL_CreateTexture(ctx[0].renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, ctx[0].view.width, ctx[0].view.height);
+	render(&ctx[0]);
 
-	ctx.texture = SDL_CreateTexture(ctx.renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, ctx.view.width, ctx.view.height);
-
-	render(&ctx);
+	// GRNN
+	if (SDL_CreateWindowAndRenderer(ctx[1].view.width, ctx[1].view.height, 0, &ctx[1].window, &ctx[1].renderer)) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Impossível criar a janela e o renderizador: %s", SDL_GetError());
+		exit(-1);
+	}
+	SDL_SetWindowTitle(ctx[1].window, "GRNN");
+	ctx[1].texture = SDL_CreateTexture(ctx[1].renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, ctx[1].view.width, ctx[1].view.height);
+	render(&ctx[1]);
 
 	// Recolher eventos
 	while (1){
-		SDL_PollEvent(&ctx.event);
-		if (ctx.event.type == SDL_QUIT){
-			finalizar(&ctx);
+		SDL_PollEvent(&ctx[0].event);
+		if (ctx[0].event.type == SDL_QUIT){
+			finalizar(&ctx[0]);
 			return 0;
 		}
-		else if( ctx.event.type == SDL_KEYDOWN ){
-			if ( ctx.event.key.keysym.sym == SDLK_q ){ 
+		else if( ctx[0].event.type == SDL_KEYDOWN ){
+			if ( ctx[0].event.key.keysym.sym == SDLK_q ){ 
 				// Tecla "q" encerra
-				finalizar(&ctx);
+				finalizar(&ctx[0]);
 				return 0;
 			}
-			else if ( ctx.event.key.keysym.sym == SDLK_UP ){ 
+			else if ( ctx[0].event.key.keysym.sym == SDLK_UP ){ 
 				// Seta pra cima
-				double dy = (ctx.ymax - ctx.ymin)*1e-1;
-				ctx.ymin += dy;
-				ctx.ymax += dy;
-				render(&ctx);
+				double dy = (ctx[0].ymax - ctx[0].ymin)*1e-1;
+				ctx[0].ymin += dy;
+				ctx[0].ymax += dy;
+				render(&ctx[0]);
 			}
-			else if ( ctx.event.key.keysym.sym == SDLK_DOWN ){ 
+			else if ( ctx[0].event.key.keysym.sym == SDLK_DOWN ){ 
 				// Seta pra baixo
-				double dy = (ctx.ymax - ctx.ymin)*1e-1;
-				ctx.ymin -= dy;
-				ctx.ymax -= dy;
-				render(&ctx);
+				double dy = (ctx[0].ymax - ctx[0].ymin)*1e-1;
+				ctx[0].ymin -= dy;
+				ctx[0].ymax -= dy;
+				render(&ctx[0]);
 			}
-			else if ( ctx.event.key.keysym.sym == SDLK_LEFT ){ 
+			else if ( ctx[0].event.key.keysym.sym == SDLK_LEFT ){ 
 				// Seta pra cima
-				double dx = (ctx.xmax - ctx.xmin)*1e-1;
-				ctx.xmin -= dx;
-				ctx.xmax -= dx;
-				render(&ctx);
+				double dx = (ctx[0].xmax - ctx[0].xmin)*1e-1;
+				ctx[0].xmin -= dx;
+				ctx[0].xmax -= dx;
+				render(&ctx[0]);
 			}
-			else if ( ctx.event.key.keysym.sym == SDLK_RIGHT ){ 
+			else if ( ctx[0].event.key.keysym.sym == SDLK_RIGHT ){ 
 				// Seta pra baixo
-				double dx = (ctx.xmax - ctx.xmin)*1e-1;
-				ctx.xmin += dx;
-				ctx.xmax += dx;
-				render(&ctx);
+				double dx = (ctx[0].xmax - ctx[0].xmin)*1e-1;
+				ctx[0].xmin += dx;
+				ctx[0].xmax += dx;
+				render(&ctx[0]);
 			}
-			else if ( ctx.event.key.keysym.sym == SDLK_z ){ 
+			else if ( ctx[0].event.key.keysym.sym == SDLK_z ){ 
 				// zoom in
-				double mx = (ctx.xmin + ctx.xmax)/2.0;
-				double my = (ctx.ymin + ctx.ymax)/2.0;
-				double dx = (ctx.xmax - ctx.xmin)/3.0;
-				double dy = (ctx.ymax - ctx.ymin)/3.0;
-				ctx.xmin = mx - dx;
-				ctx.xmax = mx + dx;
-				ctx.ymin = my - dy;
-				ctx.ymax = my + dy;
-				render(&ctx);
+				double mx = (ctx[0].xmin + ctx[0].xmax)/2.0;
+				double my = (ctx[0].ymin + ctx[0].ymax)/2.0;
+				double dx = (ctx[0].xmax - ctx[0].xmin)/3.0;
+				double dy = (ctx[0].ymax - ctx[0].ymin)/3.0;
+				ctx[0].xmin = mx - dx;
+				ctx[0].xmax = mx + dx;
+				ctx[0].ymin = my - dy;
+				ctx[0].ymax = my + dy;
+				render(&ctx[0]);
 			}
-			else if ( ctx.event.key.keysym.sym == SDLK_x ){ 
+			else if ( ctx[0].event.key.keysym.sym == SDLK_x ){ 
 				// zoom out
-				double mx = (ctx.xmin + ctx.xmax)/2.0;
-				double my = (ctx.ymin + ctx.ymax)/2.0;
-				double dx = (ctx.xmax - ctx.xmin)*0.75;
-				double dy = (ctx.ymax - ctx.ymin)*0.75;
-				ctx.xmin = mx - dx;
-				ctx.xmax = mx + dx;
-				ctx.ymin = my - dy;
-				ctx.ymax = my + dy;
-				render(&ctx);
+				double mx = (ctx[0].xmin + ctx[0].xmax)/2.0;
+				double my = (ctx[0].ymin + ctx[0].ymax)/2.0;
+				double dx = (ctx[0].xmax - ctx[0].xmin)*0.75;
+				double dy = (ctx[0].ymax - ctx[0].ymin)*0.75;
+				ctx[0].xmin = mx - dx;
+				ctx[0].xmax = mx + dx;
+				ctx[0].ymin = my - dy;
+				ctx[0].ymax = my + dy;
+				render(&ctx[0]);
 			}
 		}
 		SDL_PumpEvents();
 		/*
-		if (SDL_GetMouseState(&ctx.curPos[0], &ctx.curPos[1]) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+		if (SDL_GetMouseState(&ctx[0].curPos[0], &ctx[0].curPos[1]) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
 			// Ação do mouse 
 		}
 		*/
